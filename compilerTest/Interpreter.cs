3e908a98c2e7fbbee782488ac9bfa9698a60714f
Denis Ergin <denis.ergin@outlook.de>
2021-07-21 20:40:24 +0200

    Remove unused variable

--- compilerTest/Interpreter.cs
+++ compilerTest/Interpreter.cs
@@ -16,7 +16,6 @@ namespace compilerTest
 
         private Token getNextToken()
         {
-            string[] allDigits = new string[10]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
             string text = this.text;
             if (pos > text.Length - 1)
             {

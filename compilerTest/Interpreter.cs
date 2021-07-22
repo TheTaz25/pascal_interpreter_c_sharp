a788fef818ad8f9e3e714f0c3c3ecfde7c4aa5df
Denis Ergin <denis.ergin@outlook.de>
2021-07-22 21:12:11 +0200

    Add a new class that handles all operations along the input

--- compilerTest/Interpreter.cs
+++ compilerTest/Interpreter.cs
@@ -1,82 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace compilerTest
 {
     class Interpreter
     {
-        private string text;
-        private int pos = 0;
+        private Text text;
         private Token currentToken = null;
         public Interpreter(string text)
         {
-            this.text = text;
+            this.text = new Text(text);
         }
 
         private Token getNextToken()
         {
-            if (isEOL())
+
+            if (text.isEol())
             {
                 return new Token(Token.Type.EOF, null);
             }
+            text.skipWhiteSpace();
 
-            string currentChar = text.Substring(pos++, 1);
+            if (text.isDigit())
+                return new Token(Token.Type.INTEGER, text.getNextIntegerValue());
 
-            while (currentChar == " ")
+            if(text.isMathChar())
             {
-                currentChar = text.Substring(pos++, 1);
+                if (text.isPlus())
+                    return new Token(Token.Type.PLUS, text.getChar(true));
+                if (text.isMinus())
+                    return new Token(Token.Type.MINUS, text.getChar(true));
+                if (text.isMult())
+                    return new Token(Token.Type.MULT, text.getChar(true));
+                if (text.isDiv())
+                    return new Token(Token.Type.DIV, text.getChar(true));
             }
 
-            switch (currentChar)
-            {
-                case "1":
-                case "2":
-                case "3":
-                case "4":
-                case "5":
-                case "6":
-                case "7":
-                case "8":
-                case "9":
-                case "0":
-                    return getIntegerToken(currentChar);
-                case "+":
-                    return new Token(Token.Type.PLUS, currentChar);
-                case "-":
-                    return new Token(Token.Type.MINUS, currentChar);
-                case "*":
-                    return new Token(Token.Type.MULT, currentChar);
-                case "/":
-                    return new Token(Token.Type.DIV, currentChar);
-                default:
-                    throw new Exception("Error parsing input");
-            }
-        }
-
-        private bool isEOL() => pos > text.Length - 1;
-
-        private Token getIntegerToken(string current)
-        {
-            string allIntegers = current;
-            List<string> allDigits = new List<string>() { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
-            if (!isEOL())
-            {
-                while (!isEOL())
-                {
-                    string currentChar = text.Substring(pos++, 1);
-                    if (allDigits.Contains(currentChar))
-                    {
-                        allIntegers += currentChar;
-                    }
-                    else
-                    {
-                        pos--;
-                        break;
-                    }
-                    if (isEOL()) { break; }
-                }
-            }
-            return new Token(Token.Type.INTEGER, int.Parse(allIntegers));
+            throw new Exception("Token not recognized: " + text.getChar());
         }
 
         private void eat(Token.Type type)
---
+++ compilerTest/Text.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compilerTest
+{
+    class Text
+    {
+        char[] chars;
+        int currentPosition = 0;
+        bool lastCharSent = false;
+
+        private List<char> digits = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        private List<char> mathChars = new List<char>() { '-', '+', '*', '/' };
+        public Text(string text)
+        {
+            chars = text.ToCharArray();
+        }
+
+        public bool isEol() => currentPosition == (chars.Length - 1) && lastCharSent;
+
+        public char getChar(bool advance = false) {
+            char c = chars[currentPosition];
+            if (currentPosition == (chars.Length - 1))
+            {
+                lastCharSent = true;
+            }
+            if (advance)
+            {
+                advanceChar();
+            }
+            return c;
+        }
+
+        public void advanceChar()
+        {
+            if (currentPosition != (chars.Length - 1))
+            {
+                currentPosition++;
+            }
+        }
+
+        public bool isDigit() => digits.Contains(chars[currentPosition]);
+        public bool isMathChar() => mathChars.Contains(chars[currentPosition]);
+
+        public void skipWhiteSpace()
+        {
+            while (chars[currentPosition] == ' ')
+            {
+                advanceChar();
+                if (isEol()) break;
+            }
+        }
+
+        public bool isPlus() => chars[currentPosition] == '+';
+        public bool isMinus() => chars[currentPosition] == '-';
+        public bool isMult() => chars[currentPosition] == '*';
+        public bool isDiv() => chars[currentPosition] == '/';
+
+        public int getNextIntegerValue()
+        {
+            string allIntegers = getChar().ToString();
+            advanceChar();
+            while(!isEol() && isDigit())
+            {
+                allIntegers += getChar(true);
+            }
+            return int.Parse(allIntegers);
+        }
+    }
+}
